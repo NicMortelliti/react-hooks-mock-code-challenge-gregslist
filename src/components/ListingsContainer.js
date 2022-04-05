e9deed69c4b37e3d2e1839be6e53288af23bef5e
NicMortelliti <npmortelliti@gmail.com>
2022-04-05 13:37:18 -0700

    Add useState and useEffect to ListingsContainer. Pass listings props down to ListingCard.

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,23 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, {useState, useEffect} from "react";
+import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
+  // Set up useState for storing listings array
+  const [listingsState, setListingsState] = useState([])
+
+  // Initial population of listings
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then(r => r.json())
+      .then(listingsData => setListingsState(listingsData))
+  })
+
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listingsState.map(listing => {
+          return < ListingCard listingProps={listing}/>})}
       </ul>
     </main>
   );
